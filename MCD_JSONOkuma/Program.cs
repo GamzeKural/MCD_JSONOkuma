d7a65a0261e71b1677bfc74d6503bdf296c1963b
GAMZE KURAL <gamzekural98@gmail.com>
2021-12-22 21:18:26 +0300

    Add project files.

---
+++ MCD_JSONOkuma/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MCD_JSONOkuma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string JsonOkunanData = File.ReadAllText("c:\\JsonIslemlerim\\Personellerim.json");
+            List<Personel> Data = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Personel>>(JsonOkunanData);
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine(Data[i].ID);
+                Console.WriteLine(Data[i].Isim);
+                Console.WriteLine(Data[i].Soyisim);
+                Console.WriteLine(Data[i].TelefonNumarasi);
+                Console.WriteLine(Data[i].Sehir);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
